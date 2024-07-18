b44e9f12c7b058b09bf9c6d18c06b13a2125356f
hmmini <pak4184@naver.com>
2024-07-18 19:19:42 +0900

    kafka 수신 서버 개발

---
+++ src/main/java/com/mini/joymall/JoyMallKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.mini.joymall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoyMallKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoyMallKafkaApplication.class, args);
+    }
+
+}
